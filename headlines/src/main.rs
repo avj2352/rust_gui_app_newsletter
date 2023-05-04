789eafda92a50c3e916f551a69c9c2fd90cd90eb
PRAMOD A JINGADE <avj2352@gmail.com>
2023-05-04 22:05:20 +0800

    customize fonts in centralpanel

--- headlines/src/main.rs
+++ headlines/src/main.rs
@@ -1,5 +1,5 @@
 use eframe::{
-    egui::{CentralPanel, ScrollArea},
+    egui::{CentralPanel, CtxRef, FontDefinitions, FontFamily, ScrollArea, Vec2},
     epi::App,
     run_native, NativeOptions,
 };
@@ -25,10 +25,47 @@ impl Headlines {
             articles: Vec::from_iter(iter),
         }
     }
+
+    fn configure_fonts(&self, ctx: &CtxRef) {
+        // create font definition
+        let mut font_def = FontDefinitions::default();
+        // load font
+        font_def.font_data.insert(
+            "MesloLGS".to_string(),
+            std::borrow::Cow::Borrowed(include_bytes!("../../MesloLGS_NF_Regular.ttf")),
+        );
+        // set size
+        font_def.family_and_size.insert(
+            eframe::egui::TextStyle::Heading,
+            (FontFamily::Proportional, 35.),
+        );
+        font_def.family_and_size.insert(
+            eframe::egui::TextStyle::Body,
+            (FontFamily::Proportional, 20.),
+        );
+        // tell egui to load our custom font
+        font_def
+            .fonts_for_family
+            .get_mut(&FontFamily::Proportional)
+            .unwrap()
+            .insert(0, "MesloLGS".to_string());
+        // load the font using contxt object
+        ctx.set_fonts(font_def);
+    }
 }
 
 // Main container
 impl App for Headlines {
+    // customize fonts
+    fn setup(
+        &mut self,
+        _ctx: &eframe::egui::CtxRef,
+        _frame: &mut eframe::epi::Frame<'_>,
+        _storage: Option<&dyn eframe::epi::Storage>,
+    ) {
+        self.configure_fonts(_ctx);
+    }
+
     fn update(&mut self, ctx: &eframe::egui::CtxRef, frame: &mut eframe::epi::Frame<'_>) {
         CentralPanel::default().show(ctx, |ui| {
             // create a scrollable ui widget
@@ -49,6 +86,8 @@ impl App for Headlines {
 
 fn main() {
     let app = Headlines::new();
-    let win_option = NativeOptions::default();
+    let mut win_option = NativeOptions::default();
+    // set window size
+    win_option.initial_window_size = Some(Vec2::new(540., 960.));
     run_native(Box::new(app), win_option);
 }
